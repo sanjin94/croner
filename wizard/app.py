1209eec6a5de7f9dda1bcd34b247255d6914e555
sanjin94 <sanjin.gumbarevic@gmail.com>
2023-04-26 12:59:33 +0200

    architecture schema fix
    Signed-off-by: sanjin94 <sanjin.gumbarevic@gmail.com>

--- wizard/app.py
+++ wizard/app.py
@@ -1,6 +1,7 @@
 import subprocess
 import logging
 import yaml
+import time
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
@@ -24,42 +25,31 @@ async def creation(message: Message):
     if "create_pod," in message.text.lower():
         logging.info(f"Recieved following message: {message}")
         receipt = message.split(",")
-        #  collect deployment info to inject it in yaml file
-        #+ for sending the message after task complete
-        deployment_info = collect_deployment_info()
-        #  template yaml file update with received instructions and
-        #+ deployment info (service where FastAPI is listening)
-        inject_receipt_to_yaml(receipt[1:], deployment_info)
-        logging.info(f"Commands injected: {[receipt[i] for i in range(2,len(receipt))]}")
+        #  template yaml file update with received instructions 
+        inject_receipt_to_yaml(receipt[1:])
+        logging.info(f"Commands injected: {[receipt[i] for i in range(2,len(receipt))]} to start pod {receipt[1]}")
         # applying updated yaml template
         kubectl_apply()
-        logging.info(f"Pod {receipt[1]} created: {message}")
+        logging.info(f"Pod {receipt[1]} created!")
+        clean(receipt[1])
+        logging.info(f"Pod {receipt[1]} deleted!")
     else:
         # raise an exception for unknown messages
         raise HTTPException(status_code=400, detail="Unknown message")
 
-@app.post("/terminate_pod")
-async def termination(message: Message):
-    if "terminate_pod," in message.text.lower():
-        logging.info(f"Recieved following message: {message}")
-        # parsing pod name from message and calling delete method
-        kubectl_delete(message.split(",")[1])
-        logging.info(f"Pod {message.split(',')[1]} deleted")
-    else:
-        # raise an exception for unknown messages
-        raise HTTPException(status_code=400, detail="Unknown message")
+def clean(pod_name):
+    while True:
+        svc_raw = subprocess.check_output(["kubectl", "get", "pods"]).strip()
+        svc_str = svc_raw.decode("utf-8")
+        svc_list = [line.split() for line in svc_str.splitlines()]
+        target_list = [row for row in svc_list if row[0].startswith(pod_name)][0]
+        if target_list[2] == "Completed":
+            kubectl_delete(pod_name)
+            return
+        else:
+            time.sleep(60)
 
-def collect_deployment_info():
-    svc_raw = subprocess.check_output(["kubectl", "get", "services"]).strip()
-    svc_str = svc_raw.decode("utf-8")
-    svc_list = [line.split() for line in svc_str.splitlines()]
-    target_list = [row for row in svc_list if row[0].startswith("croner-wizard")][0]
-    #  needed information is cluster-ip and port so the service can send termination
-    #+ message after job is complete... returning ['cluster-ip', 'port_number']
-    logging.info(f"Application service {target_list[0]} can be accessed on {target_list[2]}:{target_list[2].split(':')[0].split('/')[0]}")
-    return [target_list[2], target_list[2].split(':')[0].split('/')[0]]
-
-def inject_receipt_to_yaml(receipt, deployment_info):
+def inject_receipt_to_yaml(receipt):
     with open("templates/temp_pod.yaml", "r") as f:
         yaml_template = f.read()
     # termination message to be sent after task is finished
@@ -68,10 +58,7 @@ def inject_receipt_to_yaml(receipt, deployment_info):
     # defining pod name
     yaml_dict['metadata']['name'] = receipt[0]
     # injection of commands that will run in pod
-    yaml_dict['spec']['containers'][0]['args'] = [
-                                                    " ".join(receipt[1:]), 
-                                                    f"curl -XPOST -d {message} {deployment_info[0]}:{deployment_info[1]}"
-                                                ]
+    yaml_dict['spec']['containers'][0]['args'] = [" ".join(receipt[1:])]
     # saving template so it can be applyed
     with open("templates/temp_pod.yaml", "w") as f:
        f.write(yaml.dump(yaml_dict))
